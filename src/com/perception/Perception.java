c6a8cf0d54e2a29898f00c91e8c4a3a8df553e47
Tony Kronqvist <tonykronqvist@ladmins-MacBook-Pro.local>
2016-12-21 17:27:50 +0100

    Version 0.0.1 a

--- src/com/perception/Perception.java
+++ src/com/perception/Perception.java
@@ -1,5 +1,7 @@
 package com.perception;
 ;
+import com.sun.tools.javac.jvm.ClassFile;
+
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
@@ -8,34 +10,46 @@ import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
 public class Perception extends JPanel {
+
+    private static String VERSION = "0.0.1 a";
     int x = 0;
     int y = 0;
-    private void moveBall() {
-        x = x + 1;
-        y = y + 1;
-    }
-    @Override
-    public void paint(Graphics g) {
-        super.paint(g);
-        Graphics2D g2d = (Graphics2D) g;
-        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
-                RenderingHints.VALUE_ANTIALIAS_ON);
-        g2d.fillOval(x, y, 30, 30);
-    }
+
 
     public static void main(String[] args) throws InterruptedException {
-        JFrame frame = new JFrame("Sample Frame");
+
+        JFrame frame = new JFrame("Perception - " + VERSION);
         Perception game = new Perception();
+
         frame.add(game);
         frame.setSize(300, 400);
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+
+        // Game loop
         while (true) {
             game.moveBall();
             game.repaint();
             Thread.sleep(10);
         }
     }
+
+
+    private void moveBall() {
+        x = x + 1;
+        y = y + 1;
+    }
+
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);
+        g2d.fillOval(x, y, 30, 30);
+    }
+
+
 }
 
