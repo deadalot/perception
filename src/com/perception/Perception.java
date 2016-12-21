9490b6d0d78fd1a2204d803eda8de9ea65b52774
John <john_r_lindstrom@outlook.com>
2016-12-21 20:03:00 +0100

    Added flying by console

--- src/com/perception/Perception.java
+++ src/com/perception/Perception.java
@@ -1,9 +1,11 @@
 package com.perception;
+
 ;
 
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
+import java.util.Scanner;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
@@ -36,8 +38,18 @@ public class Perception extends JPanel {
 
 
     private void moveBall() {
-        x = x + 1;
-        y = y + 1;
+        int moveRight = 0;
+        int moveOther = 0;
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        if (input.contains("e")) {
+            moveRight++;
+        }
+        if (input.contains("f")) {
+            moveOther++;
+        }
+        x += moveRight;
+        y += moveOther;
     }
 
 
@@ -45,8 +57,8 @@ public class Perception extends JPanel {
     public void paint(Graphics g) {
         super.paint(g);
         Graphics2D g2d = (Graphics2D) g;
-        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);
-        g2d.fillOval(x, y, 30, 30);
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2d.fillOval(x, y, 90, 30);
     }
 
 
